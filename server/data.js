eff06a96e6cf5ea10595922482068110eeaa69ea
edisemin <edisemin@hotmail.com>
2022-07-20 09:55:13 +0100

    Adding initial server side structure

---
+++ server/data.js
@@ -0,0 +1,6 @@
+const players = [
+    {id: 1, firstName: "Diego", lastName: "Maradona", isAlive: false, DOB: "October 30, 1960", DOD: "25 November, 2020", height: "5 ft 5 in", playingPosition: "Striker"},
+    {id: 2, firstName: "Pele", lastName: "Edson Arantes do Nascimento", isAlive: true, DOB: "October 23, 1940", height: "5 ft 8 in", playingPosition: "Striker"},
+    {id: 3, firstName: "Johann", lastName: "Cruyff", isAlive: false, DOB: "April 25, 1947", DOD: "March 24, 2016", height: "5 ft 10 in", playingPosition: "attacking midfielder"},
+    {id: 4, firstName: "Pele", lastName: "Edson Arantes do Nascimento", isAlive: true, DOB: "October 23, 1940", height: "5 ft 8 in", playingPosition: "Striker"},
+]
